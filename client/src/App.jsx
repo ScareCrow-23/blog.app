9c5a7a4df88a9791bffa587faf62139ef6dd99a6
Saumil Dhumal <dhumalsaumil@gmail.com>
2024-02-03 14:24:45 +0530

    Created Header Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,11 @@ import Contact from "./pages/Contact";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
+import Header from "./components/Header";
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,55 @@
+import { Button, Navbar, TextInput } from "flowbite-react";
+import { Link, useLocation } from "react-router-dom";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa";
+import logo from "../assets/logo.png";
+export default function () {
+  const path = useLocation().pathname;
+  return (
+    <Navbar className="border-b-2">
+      <Link
+        to="/"
+        className="self-centered whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+      >
+        <img
+          src={logo}
+          alt=""
+          className="h-12 w-12 bg-black rounded-full object-contain object-center overflow-hidden "
+        />
+        {/* <span className="bg-[#008080] text-white m-1 px-2 rounded">Dev</span>
+        Dose */}
+      </Link>
+      <form>
+        <TextInput
+          type="text"
+          placeholder="Search"
+          rightIcon={AiOutlineSearch}
+          className="hidden lg:inline"
+        />
+      </form>
+      <Button className="w-12 h-10 lg:hidden" color="gray" pill>
+        <AiOutlineSearch />
+      </Button>
+      <div className="flex gap-2 md:order-2">
+        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
+          <FaMoon />
+        </Button>
+        <Link to={"/sign-in"}>
+          <Button outline>Sign In</Button>
+        </Link>
+        <Navbar.Toggle></Navbar.Toggle>
+      </div>
+      <Navbar.Collapse>
+        <Navbar.Link active={path === "/"} as={"div"}>
+          <Link to="/">Home</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/about"} as={"div"}>
+          <Link to="/about">About</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/contact"} as={"div"}>
+          <Link to="/contact">Contact</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
