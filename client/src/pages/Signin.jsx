b8dfe1a23e4bd5b7fecb5c32e9115ccf3df39fa6
Saumil Dhumal <dhumalsaumil@gmail.com>
2024-02-05 13:55:52 +0530

    Added Functionality to sign in page

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,5 +1,103 @@
-import React from "react";
+import React, { useState } from "react";
+import logo from "../assets/logo.png";
+import { Link, useNavigate } from "react-router-dom";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 
 export default function Signin() {
-  return <div>Signin</div>;
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setloading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.email || !formData.password) {
+      return setErrorMessage("Please Fill Out All Fields");
+    }
+    try {
+      setloading(true);
+      setErrorMessage(null);
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setloading(false);
+      if (res.ok) {
+        navigate("/");
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setloading(false);
+    }
+  };
+  return (
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        {/* Left Div */}
+        <div className="flex-1  flex justify-center items-center flex-col">
+          <Link to="/" className="  text-sm  font-bold dark:text-white">
+            <img
+              src={logo}
+              alt=""
+              className="h-28 bg-black rounded-full object-contain object-center overflow-hidden "
+            />
+            {/* <span className="bg-[#008080] text-white m-1 px-2 rounded">Dev</span>
+        Dose */}
+          </Link>
+          <p className="font-bold mt-5"> Code. Share. Connect</p>
+        </div>
+        {/* Right Div */}
+        <div className="flex-1">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+            <div className="">
+              <Label value="Your Email" />
+              <TextInput
+                type="email"
+                placeholder="Email"
+                id="email"
+                onChange={handleChange}
+              />
+            </div>
+            <div className="">
+              <Label value="Your Password" />
+              <TextInput
+                type="password"
+                placeholder="Password"
+                id="password"
+                onChange={handleChange}
+              />
+            </div>
+            <Button className="bg-[#008080]" type="submit" disabled={loading}>
+              {loading ? (
+                <>
+                  <Spinner size="sm" />
+                  <span className="pl-3">Loading...</span>
+                </>
+              ) : (
+                "Sign In"
+              )}
+            </Button>
+          </form>
+          <div className="flex gap-2 mt-3 text-smonChange={handleChange}">
+            <span>Don't Have an Account?</span>
+            <Link to="/sign-up" className="text-blue-500">
+              Sign Up
+            </Link>
+          </div>
+          {errorMessage && (
+            <Alert className="mt-5 " color="failure">
+              {errorMessage}
+            </Alert>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
