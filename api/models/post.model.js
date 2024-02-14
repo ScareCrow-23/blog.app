01a213a36ae11fdfeb7891cf7f980eaf265ee3b0
Saumil Dhumal <dhumalsaumil@gmail.com>
2024-02-14 14:09:28 +0530

    created api route for create post

---
+++ api/models/post.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      require: true,
+    },
+    content: {
+      type: String,
+      require: true,
+    },
+    title: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    image: {
+      type: String,
+      default:
+        "https://i.pinimg.com/564x/ed/f5/dc/edf5dccbce70efe5e11efe8b9dea9c22.jpg",
+    },
+    category: {
+      type: String,
+      default: "uncategorized",
+    },
+    slug: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
