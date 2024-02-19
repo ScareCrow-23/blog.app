dd43a86b7f834e4f72530be024bc6eec9de7e7d5
Saumil Dhumal <dhumalsaumil@gmail.com>
2024-02-19 19:17:03 +0530

    show comment functionality added

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -18,8 +18,7 @@ const userSchema = new mongoose.Schema(
     },
     profilePicture: {
       type: String,
-      default:
-        "https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_1280.jpg",
+      default: "../Assets/user.jpg",
     },
     isAdmin: {
       type: Boolean,
