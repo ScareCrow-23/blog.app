39c769f9e8fb6c41deca535882cd4fb4d0e42a34
Saumil Dhumal <dhumalsaumil@gmail.com>
2024-03-01 20:12:59 +0530

    Final version. Ready to Deploy

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,40 @@
 import React from "react";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font-semibold text-center my-7">
+            About Dev Dose
+          </h1>
+          <div className=" text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
+              voluptate dicta quis, magni voluptatibus architecto perferendis.
+              In ut asperiores aliquam nisi laboriosam ducimus aperiam repellat
+              accusamus cum voluptatum tempore mollitia fugiat, saepe facilis.
+              Quae sint cum eaque ullam, pariatur quis placeat quod accusamus
+              quisquam quaerat enim tempore molestiae amet ipsam nihil est aut,
+              vitae atque tenetur soluta dicta dolorum.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+              laborum doloribus nesciunt quis dolor quam. Saepe unde consequatur
+              praesentium dolores, omnis dolor, nulla voluptatibus ducimus vitae
+              accusamus ipsum iure repellendus est cumque reprehenderit ipsam.
+              Culpa ab beatae earum fugit voluptates.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+              laborum doloribus nesciunt quis dolor quam. Saepe unde consequatur
+              praesentium dolores, omnis dolor, nulla voluptatibus ducimus vitae
+              accusamus ipsum iure repellendus est cumque reprehenderit ipsam.
+              Culpa ab beatae earum fugit voluptates.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
