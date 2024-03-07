df7f774984b4b442667bf33ec4b8f528d6b30b9f
Saumil Dhumal <dhumalsaumil@gmail.com>
2024-03-07 13:34:04 +0530

    fixed errors

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -31,7 +31,7 @@ export default function Home() {
       <div className=" p-3 bg-amber-100">
         <CallToAction />
       </div>
-      <div className="max-w-full mx-auto p-24 flex flex-col gap-8 py-7">
+      <div className="max-w-full mx-auto lg:p-24 p-3 flex flex-col gap-8 py-7">
         {posts && posts.length > 0 && (
           <div className=" flex flex-col gap-6 ">
             <h2 className="text-2xl font-semibold text-center">Recent Posts</h2>
--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -92,7 +92,7 @@ export default function PostPage() {
       <CommentsSection postId={post._id} />
       <div className="flex flex-col justify-center items-center mb-5">
         <h1 className=" text-xl mt-5">Recent Posts</h1>
-        <div className="flex flex-warp gap-5 mt-5 justify-center">
+        <div className="flex flex-wrap gap-5 mt-5 justify-center">
           {recentPosts &&
             recentPosts.map((post) => <PostCard key={post._id} post={post} />)}
         </div>
